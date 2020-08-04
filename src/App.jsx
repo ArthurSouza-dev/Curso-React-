ed3ffdef3cf668842d3081db1a7a9ff272eee89a
alessandro souza <arthursouzadevel@gmail.com>
2020-08-04 15:17:04 -0300

    Adicionado Aleatorio e Formatacao

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,26 @@ import React from 'react'
 import Primeiro from './componts/basicos/Primeiro'
 import ComParametro from './componts/basicos/ComParamentro'
 import Fragmento from './componts/basicos/Fragmento'
-
+import Aleatorio from './componts/basicos/Aleatorio'
 
 
 export default function App(props){
     return(
-    <div id="app">
+        <div id="app">
+            
         <h1>Fundamnetos react</h1>
+           
+        <Aleatorio min={1} max={60}/>
+           
         <Fragmento /> 
+           
         <ComParametro
         titulo="Situaçao do aluno"
         aluno="Pedro"
         nota={9.3} />
+            
         <Primeiro></Primeiro>
     
-    </div>
-
+        </div>
     )
-
 }
